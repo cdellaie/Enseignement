77e272e8d0eb4ae722ceaeb8e920d561a95a2888
cdellaie <dellaiera.clement@gmail.com>
2015-02-28 20:52:04 +0100

    Graphes !

--- StochasticFun.R
+++ StochasticFun.R
@@ -1,4 +1,4 @@
-N=10000;
+N=100;
 
 #Stochastic fun
 # A 2-graph exponential process versus a brownian motion
@@ -44,7 +44,70 @@ x11()
 plot(sort(TT[-1]-TT[-q]),(1/(q-1))*(1:(q-1)),col='red',type='s')
 lines(seq(0,14,0.1),pexp(seq(0,14,0.1),p*2.33))
 
+# graph related process
 
+# Library igraph : install.packages(igraph)
+# How to create and plot a graph
+
+library(igraph)
+
+A=matrix(c(0,1,1,0,1,0,1,0,1,1,0,1,0,0,0,1),nrow=4)
+G=graph.adjacency(A)
+plot(G)
+title(main='Graph')
+x11()
+tkplot(G)
+
+expgraph<-function(A,m){
+	T=list()
+	d=dim(A)[1]
+	for (j in 1:d){
+			v<-sample(which(A[j,]==1),m,replace='TRUE');
+			T[[j]]<-cbind(cumsum(rexp(m,1)), rep(j,m) ,v  );
+			}
+	TT=T[[1]];
+	for (j in 2:d){TT <- rbind(TT,T[[j]])};
+	return(TT[order(TT[,1]),])
+	}
+
+dynamic <- function(A,n,x0){
+	exp <- expgraph(A,n);
+	t=exp[,1];
+	l=dim(exp)[1];
+	d=dim(A)[1];
+	X=matrix(0,l,d*n);
+
+	for (j in 1:(dim(A)[1]))
+	{X[,j] <- x0[j]+cumsum(1*(c[,3]==j))-cumsum(1*(c[,2]==j));}
+	
+	return(cbind(X,t));}
+
+N=dynamic(A,10,rep(10,4));
+
+x11();
+plot(t,N[,1],type='s')
+for (j in 2:(dim(A)[1])){lines(t,N[,j],type='s',col=j)}
+title(main='Simulation')
+
+# more vertices
+
+B=matrix(rbinom(10*10,1,0.2),nrow=10)
+GG=graph.adjacency(B)
+NN=dynamic(B,100,rep(10,10));
+dd=dim(B)[1];
+
+x11()
+plot(GG)
+title(main='Graph')
+
+x11();
+plot(NN[,dd+1],NN[,1],type='s')
+for (j in 2:(dd)){lines(NN[,dd+1],NN[,j],type='s',col=j)}
+title(main='Simulation')
+
+# using igraph...
+
+g=graph.empty();
 
 
 
